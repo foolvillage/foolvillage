b5f815e5db8862d565f2387d2c621ecea6223798
foolvillage <foolvillage@163.com>
2019-11-03 17:55:44 +0800

    上传about照片

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,14 +3,31 @@
     <div class="container starter-template">
       <h4>钢铁是怎样没有炼成的……</h4>
       <p class="lead">从未成年人渣到成年人渣！</p>
-      <div class="text-left">
+      <!-- div class="text-left">
         <vue-markdown>{{msg}}</vue-markdown>
-      </div>
+      </div-->
+      <img class="container" src="../assets/images/about/1.png" />
+      <img class="container" src="../assets/images/about/3.png" />
+      <img class="container" src="../assets/images/about/4.png" />
+      <img class="container" src="../assets/images/about/5.png" />
+      <img class="container" src="../assets/images/about/6.png" />
+      <img class="container" src="../assets/images/about/7.png" />
+      <img class="container" src="../assets/images/about/9.png" />
+      <img class="container" src="../assets/images/about/10.png" />
+      <img class="container" src="../assets/images/about/11.png" />
+      <img class="container" src="../assets/images/about/12.png" />
+      <img class="container" src="../assets/images/about/13.png" />
+      <img class="container" src="../assets/images/about/14.png" />
+      <img class="container" src="../assets/images/about/15.png" />
+      <img class="container" src="../assets/images/about/16.png" />
+      <img class="container" src="../assets/images/about/17.png" />
+      <img class="container" src="../assets/images/about/18.png" />
+      <img class="container" src="../assets/images/about/19.png" />
     </div>
   </main>
 </template>
 
-<script>
+<!-- script>
 import VueMarkdown from "vue-markdown";
 import ABOUT from "../assets/md/about.md";
 
@@ -26,4 +43,4 @@ export default {
   },
   methods: {}
 };
-</script>
+</script -->
