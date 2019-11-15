f78ed21f7365136c0f185ebe488ed64e0c8fb846
foolvillage <foolvillage@github.com>
2019-11-16 03:05:12 +0800

    fix

--- src/components/Photo_2009.vue
+++ src/components/Photo_2009.vue
@@ -7,8 +7,6 @@
       </div>
     </section>
     <div class="container starter-template">
-      <h4>留下影像的，都是记不清的场景……</h4>
-      <p class="lead">发胖、脱发、谢顶、老脸松弛的时间线！</p>
       <img class="container my-3 p-3 bg-white rounded shadow-sm" src="/static/img/2009/0.jpg" />
       <img class="container my-3 p-3 bg-white rounded shadow-sm" src="/static/img/2009/1.jpg" />
       <img class="container my-3 p-3 bg-white rounded shadow-sm" src="/static/img/2009/2.jpg" />
